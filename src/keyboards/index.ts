1c7a334a0e557dfda5b73087524db9fb1e01ae9a
Yevhenii <zhekaboy04@gmail.com>
2025-04-07 13:16:19 +0200

    Change Jakub url

--- src/keyboards/index.ts
+++ src/keyboards/index.ts
@@ -140,7 +140,7 @@ export function createManagerKeyboard(
           `buttons.${TRANSLATION_KEYS.buttons.connect}` as TranslationKey,
           { manager }
         ),
-        "https://t.me/GHub_International_Jakob"
+        "https://t.me/GHubInternational"
       );
     } else if (manager === "Vladislav") {
       keyboard.url(
